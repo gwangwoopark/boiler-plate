4ad3a7da997d9ddd7473433f2f503ea3a144fd5f
Gwangwoo Park <gwangwoo.park@gmail.com>
2021-07-30 16:47:17 +0900

    Secure Information

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://gwangwoo:gwangwoo@boilerplate.059xs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key')
+
+const { User } = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose') 
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! nodemon')
 })
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
